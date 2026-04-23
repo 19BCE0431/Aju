10a6f9c3f2025946f1ca2c8cd7007c514971303d
Mohit sai krishna peddakotla <67175641+19BCE0431@users.noreply.github.com>
2026-04-23 07:55:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ API_URL = "https://aju-production.up.railway.app"
 
 st.title("📊 Financial PDF Smart Search")
 
-# Upload PDF
+# ---------------------------
+# UPLOAD
+# ---------------------------
 file = st.file_uploader("Upload your PDF")
 
 if file:
@@ -19,14 +21,17 @@ if file:
     else:
         st.error("❌ Upload failed")
 
-# Search
+
+# ---------------------------
+# SEARCH
+# ---------------------------
 query = st.text_input("🔍 Search (example: chethana)")
 
 if query:
     res = requests.get(f"{API_URL}/search", params={"q": query})
 
     if res.status_code == 200:
-        response = res.json()   # ✅ IMPORTANT
+        response = res.json()
 
         data = response.get("results", [])
 
@@ -35,7 +40,7 @@ if query:
         else:
             df = pd.DataFrame(data)
 
-            # Safe numeric conversion
+            # Ensure columns exist safely
             for col in ["debit", "credit", "balance"]:
                 if col in df.columns:
                     df[col] = pd.to_numeric(df[col], errors="coerce").fillna(0)
